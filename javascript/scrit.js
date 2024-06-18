d6cf3458ede2a6d02125f670923903cc19067027
vaibhav <139456373+Vaibhav1907@users.noreply.github.com>
2024-06-18 15:08:56 +0530

    update

---
+++ javascript/scrit.js
@@ -0,0 +1,182 @@
+// console.log("hello world!");
+// alert("hello world!");
+// documunent.write("hello world!");
+
+// let a=10;
+// const b=20;
+// var c=10;
+// console .log(a);
+// console .log(b);
+// console.log(c);
+
+
+{
+    // let a=10
+    //  console.log(a)
+    //  const  a=10 
+    // console.log(a)  
+}
+
+// let a=5;
+// let b=10;
+// let c=20;
+// if(a>b){
+//     console.log("a is greater than b");
+// }
+// else if (b>c){
+//     console.log("b is greater than c");
+// }
+// else {
+//     console.log("c is greater ");
+// }
+
+
+// for (let a=1; a<=10; a++) {
+//     console.log(a*3);
+// }
+
+// for (let a=1; a<=40; a++) {
+//     console.log(a*10);
+
+// }
+
+// let a=5;
+// let b=5;
+// if (a>b){
+//     console.log("a is greater than b");
+// }
+// else if (b>a){
+//     console.log("b is greater than a");
+// }
+
+
+// for(let a=0; a<10; a++){
+//     console.log(a*10);
+// }
+// let a=10;
+// let b=10;
+// if (a>b){
+//     console.log("a is greater than b")
+// }
+// else{
+//     console.log("b is greater than a")
+// }
+
+//  const number=prompt("enter the first value");
+//  const number1=prompt("enter the second value");
+//  if (number>=number1){
+//     console.log("num is grater than number1")
+//  }
+//  else {
+//     console.log("number1 is grater than num")
+//  }
+
+// const number = parseInt(prompt("Enter the first value:"));
+// const number1 = parseInt(prompt("Enter the second value:"));
+
+// if (number >= number1) {
+//   document.write(`The maximum number is: ${number}`);
+// } else {
+//    document.write(`The maximum number is: ${number1}`);
+// }
+ 
+// const number = parseInt(prompt("Enter the first number:"));
+// const number1 = parseInt(prompt("Enter the second number:"));
+// const number2 = parseInt(prompt("Enter the third number:"));
+
+// if (number >= number1 && number >= number2) {
+//   console.log(`${number} is greater than ${number1} and ${number2}`);
+// } else if (number1 >= number && number1 >= number2) {
+//   console.log(`${number1} is greater than ${number} and ${number2}`);
+// } else {
+//   console.log(`${number2} is greater than ${number} and ${number1}`);
+// }
+
+
+// const number = parseFloat(prompt("Enter a number:"));
+// if (number < 0) {
+//   console.log("The number is negative.");
+// } else if (number > 0) {
+//   console.log("The number is positive.");
+// } else {
+//   console.log("The number is zero.");
+// }
+
+
+// let number = parseInt(prompt("Enter a number:"));
+
+// if (number % 5 === 0 && number % 11 === 0) {
+//   console.log(`${number} is divisible by both 5 and 11.`);
+// } else if (number % 5 === 0) {
+//   console.log(`${number} is divisible by 5 but not by 11.`);
+// } else if (number % 11 === 0) {
+//   console.log(`${number} is divisible by 11 but not by 5.`);
+// } else {
+//   console.log(`${number} is not divisible by either 5 or 11.`);
+// }
+
+
+
+// let number = parseInt(prompt("Enter a number:"));
+
+
+// if (number === 0) {
+// console.log(`${number} is neither even nor odd.`);
+// } else if (number % 2 === 0) {
+//   console.log(`${number} is even.`);
+// } else {
+//   console.log(`${number} is odd.`);
+// }
+
+// let year = parseInt(prompt("enter the year:"))
+// if(year %4===0 && year % 100||year%400===0){
+//     console.log(`${year} is leap year.`);
+    
+// }
+// else{
+//     console.log(`${year} is not a leap year.`)
+// }
+
+
+// let character = prompt("Enter a character:");
+// if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+//   console.log(`'${character}' is an alphabet.`);
+// } else if (character >= '0' && character <= '9') {
+//   console.log(`'${character}' is a digit.`);
+// } else {
+//   console.log(`'${character}' is not an alphabet or a digit.`);
+// }
+
+// let character = prompt("Enter a character");
+// if(character==='a'||character==='e'||character==='i'||character==='o' ||character==='u'){
+//     console.log(`${character} is a vowel`);
+// }
+// else{
+//     console.log(`${character} is a consonant`);
+// }
+
+// let character = prompt("Enter a character:");
+// if ((character >= 'a' && character <= 'z') || (character >= 'A' && character <= 'Z')) {
+//   console.log(`'${character}' is an alphabet.`);
+// } else if (character >= '0' && character <= '9') {
+//   console.log(`'${character}' is a digit.`);
+// } else {
+//   console.log(`'${character}' is speacial character.`);
+// }
+
+
+
+// let character = prompt("Enter a character:");
+
+// {
+//  alert("enter the character: " + character);
+  
+//   if (character >= 'A' && character <= 'Z') {
+//     console.log(`${character} is an uppercase letter.`);
+//   } else if (character >= 'a' && character <= 'z') {
+//     console.log(`${character} is a lowercase letter.`);
+//   } else {
+//     console.log(`${character} is not an alphabet.`);
+//   }
+// }
+
